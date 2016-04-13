e5950d06a38830921ce23c1056dc9dc8dc1afe02
linanc <nli@berkeley.edu>
2016-04-13 02:02:06 -0700

    created script for generating csv

--- preprocess.py
+++ preprocess.py
@@ -1,24 +1,57 @@
+"""Some preprocessing for the author dataset. HCD+D Methods."""
 
-#script to give me more info about all the authors:
+import csv
+import sys
 
-_f = open('authornames.txt', 'r')
+def simplestats(filename):
 
-authorList = []
-lines = 0
+	f = open(filename, 'r')
+	authors = []
+	linelengths = []
+	global all_authors, num_papers
+	all_authors = [[] for x in range(11)] 
+	num_papers = 0
 
-for line in _f:
-	lines += 1
-	line.strip()
-	arrLine = line.split(",")
-	for authorname in arrLine:
-		authorname.strip()
-		authorList.append(authorname.strip('\n').lstrip())
+	for line in f:
+		line.rstrip()
+		arrLine = line.split(",")
+		count = 0
+		position = 0
 
-_f.close()
+		for name in arrLine:
+			count += 1
+			name = name.strip('\n').strip('\t').lstrip()
+			all_authors[position].append(name)
+			position += 1
+			if name not in authors:
+				authors.append(name)
 
-lengthList = len(authorList)
-print(lengthList)
-print(lines)
-print(authorList)
+		while position < 11:
+			all_authors[position].append(' ')
+			position += 1
 
-#turns out there is a total of 566 authors I need to analyze! 
+		linelengths.append(count)
+		num_papers += 1
+
+	f.close()
+	m = max(linelengths)
+	print(authors)
+	print('Max number of authors writing a single paper is: ' + str(m))
+	print('Unique number of authors is: ' + str(len(authors)))
+	print('Number of papers is: ' + str(num_papers))
+	return
+
+def writecsv(filename):
+
+	csvf = open(filename, 'wt', newline='')
+	writer = csv.writer(csvf)
+	writer.writerow(('First Author', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'))
+	for j in range(num_papers):
+	    writer.writerow((all_authors[0][j], all_authors[1][j], all_authors[2][j], all_authors[3][j], all_authors[4][j], all_authors[5][j], all_authors[6][j], all_authors[7][j], all_authors[8][j], all_authors[9][j], all_authors[10][j]))
+	csvf.close()
+	return
+
+""" On the command line, input: python preprocessing.py <yourtextfile.txt> <output.csv> """
+
+simplestats(sys.argv[1])
+writecsv(sys.argv[2])
