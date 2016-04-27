f08b1d92f92e75e586d3128d0898518375c45a57
linanc <nli@berkeley.edu>
2016-04-27 01:35:06 -0700

    prettified countries.py
    Made it more controllable on the command line and actually defined
    functions. Old text file outputs were saved as pub-old.txt and
    work-old.txt.

--- countries.py
+++ countries.py
@@ -1,4 +1,6 @@
 import csv
+import sys
+import getopt
 
 """ This is a snippet of code I wrote to regenerate the authors.txt file after deleting it.
 """
@@ -13,101 +15,190 @@ import csv
 # 		f.write(line + '\n')
 # f.close()
 
-""" Code to parse dataset, pull out relevant data (place of publication of first authors and other authors,
- and place into text file for easier processing.
-"""
+def parsedata(filename, columns):
 
-# f = open('countrydraft.txt', 'w')
+	""" Parses dataset and pulls out relevant columns from the csv data file to text file. 
 
-# with open('dataset.csv', 'r') as csvfile:
-# 	reader = csv.reader(csvfile, delimiter=',')
-# 	for row in reader:
-# 		f.write(row[10] + ' | ' + row[12] + '\n')
-# 		print(row[10] + ' ==== ' + row[12])
+		@param args: 
+					filename, a string with the path to the csv data file
+					output, string with path to desired output text file
+					columns, a list of columns in csv data file to be extracted 
+		@param output:  
+					string path to text file with desired information 
+	"""
 
-# f.close()
+	with open('out.txt', 'w') as f:
+		with open(filename, 'r') as csvfile:
+			reader = csv.reader(csvfile, delimiter=',')
+			for row in reader:
+				if len(columns) > 1:
+					retarr = []
+					for item in columns: 
+						retarr.append(row[item])
+					retval = " | ".join(retarr)
+					f.write(retval + '\n')
+					print(retval)
+				else:
+					f.write(row[columns[0]] + '\n')
+					print(row[columns[0]])
+	print("Done! Look for <out.txt> file in your directory.")
+	return 'out.txt'
 
 
-"""reads the file produced by above to create the relevant csv"""
+def publication(output):
 
-# f = open('countrydraft.txt', 'r')
+	""" Creates a CSV table of countries, number of first authors who published there,
+		and number of other authors who published there. 
 
-# firstCountries = dict()
-# otherCountries = dict()
-# check = set()
+		@param args: 
+					output, a string with the path to the text file output by parsedata
+					csvfilename, user specified string with the path to new csv file
 
-# for line in f: 
-# 	arrLine = line.split(' | ')
-# 	firstC = arrLine[0].split(',')
-# 	secC = arrLine[1].split(',')
+		@param output: 
+					a csv file with desired analysis
+					prints string path to csv file 
+	"""
 
-# 	for _ in firstC:
-# 		_ = _.lstrip().rstrip().strip('\n')
-# 		if _ in firstCountries:
-# 			firstCountries[_] += 1
-# 		else: 
-# 			firstCountries[_] = 1
-# 		check.add(_)
+	firstCountries = dict()
+	otherCountries = dict()
+	check = set()
+	count = 0
+	verify = True
 
-# 	for _ in secC:
-# 		_ = _.lstrip().rstrip().strip('\n')
-# 		if _ in otherCountries:
-# 			otherCountries[_] += 1
-# 		else: 
-# 			otherCountries[_] = 1
+	with open(output, 'r') as f: 
+		for line in f: 
+			if verify: 
+				count += 1
+				verify = False
+				continue
+				
+			if count == 1: 
+				count = 0
+				continue
 
-# print(firstCountries)
-# print(otherCountries)
+			arrLine = line.split(' | ')
+			firstC = arrLine[0].split(',')
+			secC = arrLine[1].split(',')
 
-# f.close()
+			for _ in firstC:
+				_ = _.lstrip().rstrip().strip('\n')
+				if _ in firstCountries:
+					firstCountries[_] += 1
+				else: 
+					firstCountries[_] = 1
 
-# with open('countryinfo.csv', 'w', newline='') as csvfile:
-# 	writer = csv.writer(csvfile)
-# 	writer.writerow(('Country', 'Number of First Authors', 'Number of Other Authors'))
-# 	for key in firstCountries:
-# 		if key in otherCountries:
-# 			writer.writerow((key, firstCountries[key], otherCountries[key]))
-# 		elif key not in otherCountries:
-# 			writer.writerow((key, firstCountries[key], '0'))
-# 		check.add(key)
-# 	for key in otherCountries:
-# 		if key not in check: 
-# 			if key in firstCountries:
-# 				writer.writerow((key, firstCountries[key], otherCountries[key]))
-# 			elif key not in firstCountries:
-# 				writer.writerow((key, '0', otherCountries[key]))
+			for _ in secC:
+				_ = _.lstrip().rstrip().strip('\n')
+				if _ in otherCountries:
+					otherCountries[_] += 1
+				else: 
+					otherCountries[_] = 1
 
-""" Code to parse dataset, pull out place of work done,
- and place into text file for easier processing.
-"""
+		print(firstCountries)
+		print(otherCountries)
 
-# f = open('countrydraft2.txt', 'w')
 
-# with open('dataset.csv', 'r') as csvfile:
-# 	reader = csv.reader(csvfile, delimiter=',')
-# 	for row in reader:
-# 		f.write(row[13] +'\n')
-# 		print(row[13])
+	with open('countries-pub.csv', 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(('Country', 'Number of First Authors', 'Number of Other Authors'))
 
-# f.close()
+		for key in firstCountries:
+			if key in otherCountries:
+				writer.writerow((key, firstCountries[key], otherCountries[key]))
+			elif key not in otherCountries:
+				writer.writerow((key, firstCountries[key], '0'))
+			check.add(key)
 
-""" Code to analyze the countrydraft2 file"""
+		for key in otherCountries:
+			if key not in check: 
+				if key in firstCountries:
+					writer.writerow((key, firstCountries[key], otherCountries[key]))
+				elif key not in firstCountries:
+					writer.writerow((key, '0', otherCountries[key]))
 
-countries = dict()
+	print("Done! Look for <countries-pub.csv> file in your directory.")
 
-f = open('countrydraft2.txt', 'r')
-for line in f: 
-	arrLine = line.split(',')
-	for _ in arrLine:
-		_ = _.lstrip().rstrip().strip('\n')
-		if _ in countries:
-			countries[_] += 1
-		else: 
-			countries[_] = 1
-f.close()
 
-with open('countrywork.csv', 'w', newline='') as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerow(('Country', 'Number of Mentions as Place of Work'))
-	for key in countries:
-		writer.writerow((key, str(countries[key])))
+def work(output): 
+
+	""" Creates a CSV table of countries and their number of mentions as the place of work in a paper. 
+
+		@param args: 
+					output, a string with the path to the text file output by parsedata
+					csvfilename, user specified string with the path to new csv file
+
+		@param output: 
+					a csv file with desired analysis
+					prints the string path to csv  
+	"""
+
+	countries = dict()
+	verify = True
+	count = 0
+
+	with open(output, 'r') as f: 
+		for line in f: 
+			if verify: 
+				verify = False
+				count += 1
+				continue
+
+			if count == 1: 
+				count = 0
+				continue
+
+			arrLine = line.split(',')
+			for _ in arrLine:
+				_ = _.lstrip().rstrip().strip('\n')
+				if _ in countries:
+					countries[_] += 1
+				else: 
+					countries[_] = 1
+
+	with open('countries-work.csv', 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(('Country', 'Number of Mentions as Place of Work'))
+		for key in countries:
+			writer.writerow((key, countries[key]))
+
+	print("Done! Look for <countries-work.csv> file in your directory.")
+
+
+def main():
+
+	""" Function to parse command line input. 
+	    Expects csv filename, columns, and either work or publication. 
+	    On the command line: python countries.py <dataset.csv> <[col,col]> <work or publication>
+	    Columns within the csv file are 0-indexed. 
+	    Make sure there are no spaces when specifying your desired columns. 
+	"""
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+	except getopt.error as msg:
+		print (msg)
+		print ("for help use --help")
+		sys.exit(2)
+    # process options
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			print (__doc__)
+			sys.exit(0)
+    # process arguments
+	col = args[1].strip('[').strip(']')
+	arrCol = col.split(',')
+	columns = [int(x) for x in arrCol]
+	dataset = args[0]
+	option = args[2]
+	    
+	#returns string path to output text file
+	retval = parsedata(dataset, columns)
+	if option == "work":
+	  	work(retval)
+	elif option == "publication":
+		publication(retval)
+	
+	# for arg in args:
+	#     process(arg) # process() is defined elsewhere
+
+if __name__ == "__main__":
+	main()
