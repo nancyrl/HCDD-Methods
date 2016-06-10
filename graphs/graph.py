8920df4451ae2441a4860a498d5d0d4d4a6018cc
linanc <nli@berkeley.edu>
2016-06-09 20:59:05 -0700

    added information about cut points

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,16 +1,18 @@
 import networkx as nx
 from networkx.readwrite import json_graph
-import numpy as np
 import csv
 import sys
 import matplotlib.pyplot as plt
 import json
 import pickle
+import statistics
+import copy
 
 def count_authors_and_papers(filename):
 	
 	num_papers = 0
 	linelengths = []
+
 	with open(filename, 'r') as f:
 		for line in f:
 			arrLine = line.split(",")
@@ -34,12 +36,10 @@ def convert_text_to_csv(filename, outfile):
 		for line in f:
 			arrLine = line.split(",")
 			position = 0
-
 			for name in arrLine:
 				name = name.strip('\n').strip('\t').lstrip().rstrip()
 				all_authors[position].append(name)
 				position += 1
-
 			while position < max_authors:
 				all_authors[position].append('')
 				position += 1
@@ -147,9 +147,10 @@ def generate_author_to_int_dictionary(filename):
 	author_matrix = retdict['author_matrix']
 
 	#assigning each author an integer representation
-	author_to_int = dict()
-	i = 0
-	for item in sorted(author_nodes):
+	author_to_int, i = dict(), 0
+	sorted_authors = sorted(author_nodes)
+
+	for item in sorted_authors:
 		author_to_int[item] = i
 		i += 1
 
@@ -218,7 +219,8 @@ def generate_networkX_graph_string():
 	for key in author_to_int: 
 		G.add_node(key)
 
-	for row in range(len(author_matrix)):
+	num_papers = len(author_matrix)
+	for row in range(num_papers):
 		num_authors = len(author_matrix[row])
 		for j in range(num_authors):
 			a1 = author_matrix[row][j]
@@ -258,7 +260,202 @@ def calculate_metrics():
 	# papers_per_author(author_matrix)
 	# authors_per_paper(author_matrix)
 	# density(matrix)
-	author_degrees(matrix, author_to_int)
+	# author_degrees(matrix, author_to_int)
+	# connected_components(matrix, author_to_int)
+	cut_point(author_to_int)
+
+def cut_point(author_to_int): 
+
+	adj_list = pickle.load(open("adj_list.p", "rb"))
+	list_of_ints = pickle.load(open("list_of_ints.p", "rb"))
+	v = len(adj_list)
+	final = set()
+	cut_points = set()
+
+	for cc in list_of_ints:
+		if len(cc) <= 4:
+			continue
+		cc = sorted(cc)
+		cc_graph = copy.deepcopy(adj_list)
+		not_in_cc = [x for x in range(v) if x not in cc]
+
+		for i in range(v):
+			if i in not_in_cc: 
+				cc_graph[i] = set() 
+		#graph contains only the connected component
+		print("Connected component: " + str(cc))
+		cut_points = find_cut_points_in_cc(cc_graph, cc, not_in_cc)
+
+		if not cut_points:
+			print("There are no cut points.")
+		else: 
+			print("Cut points: " + str(cut_points))
+			new_cut_points = filter_cut_points(cc_graph, cut_points, cc)
+			if not new_cut_points:
+				print("All filtered out. \n")
+			elif new_cut_points == cut_points:
+				print("No need to filter. \n")
+			else: 
+				print("Filtered cut points: " + str(new_cut_points) + "\n")
+		final = final.union(new_cut_points)
+
+	inverse_author_dict = {v: k for k, v in author_to_int.items()}
+	string = [inverse_author_dict[int] for int in final]
+
+	print("Cut points for all ccs: " + str(string))
+	
+
+def find_cut_points_in_cc(graph, cc, skips):
+
+	v = len(graph)
+	visited, prev = [False for _ in range(v)], [None for _ in range(v)]
+	discover, low = [0 for _ in range(v)], [0 for _ in range(v)]
+	cut_points = set()
+
+	def cut_dfs(start):
+
+		visited[start] = True
+		nonlocal time
+		time += 1
+		low[start] = discover[start] = time
+		for w in graph[start]:
+			if not visited[w]:
+				prev[w] = start
+				cut_dfs(w)
+				low[start] = min(low[start], low[w])
+			elif w != prev[start]: 
+				#then w is visited, and (start, w) must be a back edge
+				low[start] = min(low[start], discover[w]) 
+
+	for vertex in cc:
+		if not visited[vertex]:
+			time = 0
+			cut_dfs(vertex)
+		break
+
+	for vertex in cc:
+		if prev[vertex] == None:
+			if len(graph[vertex]) > 1:
+				cut_points.add(vertex)
+		else:
+			for w in graph[vertex]:
+				if low[w] >= discover[vertex]:
+					cut_points.add(vertex)
+
+	return cut_points
+
+def filter_cut_points(graph, cut_points, cc):
+
+	v = len(graph)
+	filtered_cut_points = set()
+
+	for cut in cut_points:
+		new_graph = copy.deepcopy(graph)
+		for edge in new_graph: 
+			if cut in edge: 
+				edge.remove(cut)
+		metavisited, small_ccs = [False for _ in range(v)], []
+		for vertex in cc:
+			if vertex != cut and not metavisited[vertex]:
+				ret = dfs(metavisited, vertex, new_graph)
+				small_ccs.append(ret[0])
+				metavisited = ret[1]
+		if len(small_ccs) >= 2:
+			filtered_cut_points.add(cut)
+		print("Cut vertex: " + str(cut))
+		print(str(small_ccs))
+
+	return filtered_cut_points
+
+def dfs(metavisited, start, graph):
+	
+	visited, stack = set(), [start]
+	while stack:
+		vertex = stack.pop()
+		if vertex not in visited: 
+			metavisited[vertex] = True
+			visited.add(vertex)
+			stack.extend(graph[vertex] - visited)
+	return [visited, metavisited]
+	
+def connected_components(matrix, author_to_int):
+
+	#convert matrix to adj_list
+	v = len(matrix)
+	adj_list = convert_matrix_to_adj_list(matrix, v)
+
+	#using dfs to find connected components
+	metavisited, list_of_ints = [False for _ in range(v)], []
+
+	for vertex in range(v):
+		if not metavisited[vertex]:
+			ret = dfs(metavisited, vertex, adj_list)
+			list_of_ints.append(ret[0])
+			metavisited = ret[1]
+
+	largest = largest_connected(list_of_ints)
+	max_cc_size, max_cc, sizes = largest[0], largest[1], sorted(largest[2])
+
+	#convert components of ints into authors
+	inverse_author_dict = {v: k for k, v in author_to_int.items()}
+	list_of_strings, max_cc_string = [], []
+	
+	for cc in list_of_ints:
+		string_cc = [inverse_author_dict[i] for i in cc]
+		if cc == max_cc:
+			max_cc_string = string_cc
+		list_of_strings.append(string_cc)
+
+	#statistics of cc sizes
+	average = statistics.mean(sizes)
+	median = statistics.median(sizes)
+	median_low = statistics.median_low(sizes)
+	median_high = statistics.median_high(sizes)
+	mode = statistics.mode(sizes)
+
+	#saving info into a text file
+	with open('connected_components.txt', 'w') as f: 
+		f.write('Connected authors: ' + '\n')
+		for cc in list_of_strings:
+			f.write(str(len(cc)) + '   ' + str(cc) + '\n')
+		f.write('\n')
+		f.write('Connected nodes: ' + '\n')
+		for cc in list_of_ints:
+			f.write(str(len(cc)) + '   ' + str(cc) + '\n')
+		f.write('\n')
+		f.write('Number of connected components: ' + str(len(list_of_ints)) + '\n')
+		f.write('Largest component size: ' + str(max_cc_size) + '\n')
+		f.write('Largest connected component: ' + str(max_cc_string) + '\n')
+		f.write('Component sizes: ' + str(sizes) + '\n')
+		f.write('Average component size: ' + str(average) + '\n')
+		f.write('Median: ' + str(median) + '\n')
+		f.write('Low median: ' + str(median_low) + '\n')
+		f.write('High median: ' + str(median_high) + '\n')
+		f.write('Mode: ' + str(mode))
+
+	pickle.dump(adj_list, open("adj_list.p", "wb"))
+	pickle.dump(list_of_ints, open("list_of_ints.p", "wb"))
+
+def convert_matrix_to_adj_list(matrix, size):
+
+	adj_list = []
+	for i in range(size):
+		neighbors = set()
+		for j in range(size): 
+			if matrix[i][j] == 1:
+				neighbors.add(j)
+		adj_list.append(neighbors)
+	return adj_list
+
+def largest_connected(list_of_ints):
+
+	sizes, s = dict(), []
+	for list in list_of_ints:
+		i = len(list) 
+		sizes[i] = list
+		s.append(i)
+	m = max(sizes.keys())
+	return [m, sizes[m], s]
 
 def density(matrix):
 
